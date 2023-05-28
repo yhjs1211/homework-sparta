217c3b77068828f0d4f37c3f7dde867ed26255e5
yhjs1211 <yhjs1211@gmail.com>
2023-05-28 12:47:56 +0900

    select

--- js/button.js
+++ js/button.js
@@ -1,20 +1,36 @@
 function findMovieButton(){
     let inputText = document.querySelector("#findMovieInput").value.toLowerCase();
-    matchMovie(inputText);
+    let select = document.querySelector('#selector').value;
+    
+    matchMovie(inputText,select);
 }
 
-matchMovie = (inputText) =>{
-    let movies = document.querySelectorAll("#movieTitle");
-    let arr = []
-    movies.forEach(m=>{
-        let title = m.textContent.replace('제목 : ','').toLowerCase();
-        if(!title.includes(inputText)){
-            let displayNone = m.closest('.flip-box');
-            displayNone.setAttribute("style","display: none;");
-        }
-    })
+matchMovie = (inputText,select) =>{
+    // Title 검색
+    if(select=='titleOfMovie'){
+        let movies = document.querySelectorAll("#movieTitle");
+        let arr = []
+        movies.forEach(m=>{
+            let title = m.textContent.replace('제목 : ','').toLowerCase();
+            if(!title.includes(inputText)){
+                let displayNone = m.closest('.flip-box');
+                displayNone.setAttribute("style","display: none;");
+            }
+        })
+        return arr;
+    // ID 검색
+    }else{
+        let divs = document.querySelectorAll('.flip-box');
+        let arr = [];
+        divs.forEach(m=>{
+            let id = m.getAttribute('id');
+            if(id!=inputText){
+                m.setAttribute("style","display: none;");
+            }
+        })
+        return arr;
+    }
     
-    return arr;
 }
 
 function alertId(e){
